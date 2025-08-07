4baa0b001080721a4002aadd0343ece0833713c2
DesarrolladorWebFrias <luisfriasdesarrollador@gmail.com>
2025-08-07 15:46:02 -0600

    CREACION DEL PROYECTO CON BACKGROUND

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900 bg-cover bg-center bg-fixed" style="background-image: url('{{ asset('images/fondo5.png') }}');">
+        <div class="min-h-screen bg-transparent">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
